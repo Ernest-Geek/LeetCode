fc4b8cf902007376dcea6020223f376e874629cc
Ernest-Geek <ernestampene1@gmail.com>
2024-03-25 02:33:12 +0000

    update

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,12 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         #habdle negative numbers
-        if x <= 0:
+        if x < 0:
             return False
         
         #convert the string into an integer
         str_x = str(x)
         #returns a reverse string
-        return str_x == str_x[::-1]
-solu = Solution()
-print(solu.isPalindrome(121))
+        return str_x == str_x[::-1] or str_x == "0"
